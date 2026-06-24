b20db53a178a54d73314be1e8e6ede7576739d34
Stas Gorelik <stpjp@mail.ru>
2026-06-24 19:56:00 +0300

    Fix trash folders and HW

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -3,18 +3,25 @@ from playwright.sync_api import Page, expect
 
 
 class DashboardPage(BasePage):
-    
     def __init__(self, page: Page):
         super().__init__(page)
         self.page = page
         self.path = "dashboard"
 
         self.dashboard_title = self.page.locator('[data-qa="dashboard-title"]')
-        self.dashboard_create_board_button = self.page.locator('[data-qa="dashboard-create-board-button"]')    
-        self.brand_title = self.page.locator('.brand-title')
-        self.header_username = self.page.locator('[data-qa="header-username"]').filter(has_text="bob_user")
-        self.header_user_dropdown_info = self.page.locator('.header-user-dropdown-info')
-        self.header_logout_button = self.page.locator('[data-qa="header-logout-button"]')
+        self.dashboard_create_board_button = self.page.locator(
+            '[data-qa="dashboard-create-board-button"]'
+        )
+        self.brand_title = self.page.locator(".brand-title")
+        self.header_username = self.page.locator('[data-qa="header-username"]').filter(
+            has_text="bob_user"
+        )
+        self.header_user_dropdown_info = self.page.locator(".header-user-dropdown-info")
+        self.header_logout_button = self.page.locator(
+            '[data-qa="header-logout-button"]'
+        )
+        self.sidebar_boards_link = self.page.locator('[data-qa="sidebar-boards-link"]')
+        self.sidebar_tasks_link = self.page.locator('[data-qa="sidebar-tasks-link"]')
 
     def open(self):
         self.goto(self.path)
@@ -35,3 +42,9 @@ class DashboardPage(BasePage):
 
     def header_logout_button_click(self):
         self.header_logout_button.click()
+
+    def click_sidebar_boards_link(self):
+        self.sidebar_boards_link.click()
+
+    def sidebar_tasks_link_click(self):
+        self.sidebar_tasks_link.click()
--- tests/test_page_object.py
+++ tests/test_page_object.py
@@ -12,40 +12,45 @@ def test_dashboard_page_opened(page: Page):
     authorization.open()
     authorization.verify_that_empty_page_opened()
     authorization.autorization_fill_out_of_form()
-    page.wait_for_timeout(3000)
+
     authorization.autorization_submit_button_click()
-        
+
     dashboard_page = DashboardPage(page)
-    
+
     dashboard_page.verify_that_empty_page_opened()
+
 
 @pytest.mark.only2
 def test_boards_page_opened(page: Page):
     authorization = LoginPage(page)
+    dashboard_page = DashboardPage(page)
+    boards_page = BoardsPage(page)
     authorization.open()
     authorization.verify_that_empty_page_opened()
     authorization.autorization_fill_out_of_form()
 
     authorization.autorization_submit_button_click()
-        
-    boards_page = BoardsPage(page)
-    boards_page.open()
-    
+
+    dashboard_page.click_sidebar_boards_link()
+
     boards_page.verify_that_empty_page_opened()
+
 
 @pytest.mark.only3
 def test_tasks_page_opened(page: Page):
     authorization = LoginPage(page)
+    tasks_page = TasksPage(page)
+    dashboard_page = DashboardPage(page)
     authorization.open()
     authorization.verify_that_empty_page_opened()
     authorization.autorization_fill_out_of_form()
 
     authorization.autorization_submit_button_click()
-    page.wait_for_timeout(3000)    
-    tasks_page = TasksPage(page)
-    tasks_page.open()
+
+    dashboard_page.sidebar_tasks_link_click()
 
     tasks_page.verify_that_empty_page_opened()
+
 
 @pytest.mark.only4
 def test_authorization(page: Page):
@@ -54,10 +59,11 @@ def test_authorization(page: Page):
     authorization.verify_that_empty_page_opened()
     authorization.autorization_fill_out_of_form()
     authorization.autorization_submit_button_click()
-        
-    dashboard_page = DashboardPage(page)    
-    
+
+    dashboard_page = DashboardPage(page)
+
     dashboard_page.verify_that_empty_page_opened()
+
 
 @pytest.mark.only5
 def test_authorization_user_dropdown_info_is_visible(page: Page):
@@ -66,12 +72,13 @@ def test_authorization_user_dropdown_info_is_visible(page: Page):
     authorization.verify_that_empty_page_opened()
     authorization.autorization_fill_out_of_form()
     authorization.autorization_submit_button_click()
-        
-    dashboard_page = DashboardPage(page)    
+
+    dashboard_page = DashboardPage(page)
 
     dashboard_page.user_section_is_visible()
     dashboard_page.user_section_click()
     dashboard_page.user_section_dropdown_is_visible()
+
 
 @pytest.mark.only6
 def test_quit(page: Page):
@@ -80,13 +87,24 @@ def test_quit(page: Page):
     authorization.verify_that_empty_page_opened()
     authorization.autorization_fill_out_of_form()
     authorization.autorization_submit_button_click()
-        
-    dashboard_page = DashboardPage(page)    
+
+    dashboard_page = DashboardPage(page)
 
     dashboard_page.user_section_is_visible()
     dashboard_page.user_section_click()
-    page.wait_for_timeout(3000)
     dashboard_page.header_logout_button_click()
 
 
-        
+# @pytest.mark.only2
+# def test_boards_page_opened(page: Page):
+#     authorization = LoginPage(page)
+#     authorization.open()
+#     authorization.verify_that_empty_page_opened()
+#     authorization.autorization_fill_out_of_form()
+
+#     authorization.autorization_submit_button_click()
+
+#     boards_page = BoardsPage(page)
+#     boards_page.open()
+
+#     boards_page.verify_that_empty_page_opened()
